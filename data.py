8e091db044ef0a25971249cceb8457db239b2aa2
Naitik Kumar <naitikkumar6654@gmail.com>
2025-12-05 19:37:17 +0530

    Initial Commit

---
+++ data.py
@@ -0,0 +1,17 @@
+#Aik Software Kya Hota Ha 
+#software = CODE + DATA
+#ISSIKO BOLTE HA SOFTWARE
+
+#KUCH EXAMPLES DETA HU
+ #example 1
+
+num = int(input("Enter Any Number")) 
+num1 = int(input("Enter Any Number"))
+sum = num + num1
+print(sum)
+
+#ab samjha ki bina data ke code ka koi fayda nahi 
+#code likha hi jata ha taki data ke sath kaam kiya ja sake
+
+#data ke types
+#organised aur unorganised
